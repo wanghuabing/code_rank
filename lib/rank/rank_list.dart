3fcc5e69882d9502c5b3ba91e90045eddf202452
Canv <hbwang1314@gmail.com>
2018-09-13 00:28:45 +0800

    Rank Page Demo

--- lib/rank/rank_list.dart
+++ lib/rank/rank_list.dart
@@ -1,10 +1,80 @@
 import 'package:flutter/material.dart';
 
-class RankPage extends StatelessWidget {
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class RankPage extends StatefulWidget {
   @override
   Widget build(BuildContext ctxt) {
     return new Scaffold(
       body: Center(child: Text('First Page!')),
     );
   }
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _RankPageState();
+  }
+
+}
+
+class _RankPageState  extends State<RankPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Sample App"),
+        ),
+        body: getBody());
+  }
+
+  List widgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  showLoadingDialog() {
+    if (widgets.length == 0) {
+      return true;
+    }
+
+    return false;
+  }
+
+  getBody() {
+    if (showLoadingDialog()) {
+      return getProgressDialog();
+    } else {
+      return getListView();
+    }
+  }
+
+  getProgressDialog() {
+    return new Center(child: new CircularProgressIndicator());
+  }
+
+  ListView getListView() => new ListView.builder(
+      itemCount: widgets.length,
+      itemBuilder: (BuildContext context, int position) {
+        return getRow(position);
+      });
+
+  Widget getRow(int i) {
+    return new Padding(padding: new EdgeInsets.all(10.0), child: new Text("Row ${widgets[i]["title"]}"));
+  }
+
+  loadData() async {
+    String dataURL = "https://jsonplaceholder.typicode.com/posts";
+    http.Response response = await http.get(dataURL);
+    setState(() {
+      widgets = json.decode(response.body);
+    });
+  }
+
+
 }
