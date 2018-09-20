a585fc96d266cbb87dbe06182afe6d0542d4070f
0096001084 <0096001084@zte.com.cn>
2018-09-20 16:37:16 +0800

    1、数据获取

--- lib/model/rank_data.dart
+++ lib/model/rank_data.dart
@@ -1,22 +1,16 @@
 import 'package:code_rank/model/rank_project.dart';
 
-class RankData{
+class RankData {
   BigInt total_count;
   bool incomplete_results;
   List<RankProject> items;
 
-  RankData({
-    this.total_count,
-    this.incomplete_results,
-    this.items
-  });
+  RankData({this.total_count, this.incomplete_results, this.items});
 
-
-  factory RankData.fromJson(Map<String, dynamic> parsedJson){
+  factory RankData.fromJson(Map<String, dynamic> json) {
     return RankData(
-        total_count: parsedJson['total_count'],
-        incomplete_results : parsedJson['incomplete_results'],
-        items : parsedJson ['items']
-    );
+        total_count: json['total_count'],
+        incomplete_results: json['incomplete_results'],
+        items: json['items']);
   }
 }
--- lib/ui/rank/rank_list.dart
+++ lib/ui/rank/rank_list.dart
@@ -1,61 +1,29 @@
-import 'package:flutter/material.dart';
-import 'package:code_rank/model/rank_data.dart';
+import 'dart:async';
 import 'dart:convert';
+
+import 'package:code_rank/model/rank_data.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class RankPage extends StatefulWidget {
-  @override
-  Widget build(BuildContext ctxt) {
-    return new Scaffold(
-      body: Center(child: Text('First Page!')),
-    );
-  }
+class RankPage extends StatelessWidget {
+  List widgets = [];
 
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _RankPageState();
-  }
-
-}
-
-class _RankPageState  extends State<RankPage>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Sample App"),
-        ),
-        body: getBody());
-  }
+        body: new FutureBuilder<RankData>(
+            future: fetchRankProjects(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return getListView();
+              } else if (snapshot.hasError) {
+                return new Text("${snapshot.error}");
+              }
 
-  List widgets = [];
-
-  @override
-  void initState() {
-    super.initState();
-    loadData();
-  }
-
-  showLoadingDialog() {
-    if (widgets.length == 0) {
-      return true;
-    }
-
-    return false;
-  }
-
-  getBody() {
-    if (showLoadingDialog()) {
-      return getProgressDialog();
-    } else {
-      return getListView();
-    }
-  }
-
-  getProgressDialog() {
-    return new Center(child: new CircularProgressIndicator());
+              // By default, show a loading spinner
+              return new CircularProgressIndicator();
+            }));
   }
 
   ListView getListView() => new ListView.builder(
@@ -65,18 +33,15 @@ class _RankPageState  extends State<RankPage>{
       });
 
   Widget getRow(int i) {
-    return new Padding(padding: new EdgeInsets.all(10.0), child: new Text("Row ${widgets[i]["title"]}"));
+    return new Padding(
+        padding: new EdgeInsets.all(10.0),
+        child: new Text("Row ${widgets[i]["title"]}"));
   }
 
-  loadData() async {
-    String dataURL = "https://api.github.com/search/repositories?q=language:java&sort=stars";
-    http.Response response = await http.get(dataURL);
-    final jsonResponse = json.decode(response.body);
-    RankData data = new RankData.fromJson(jsonResponse);
-//    setState(() {
-//      widgets = json.decode(response.body);
-//    });
+  Future<RankData> fetchRankProjects() async {
+    final response = await http.get(
+        'https://api.github.com/search/repositories?q=language:java&sort=stars');
+    final responseJson = json.decode(response.body);
+    return new RankData.fromJson(responseJson);
   }
-
-
 }
