eb4ac59da1f391bd4a9a99fbc3e5a83233b4a5b7
0096001084 <0096001084@zte.com.cn>
2018-10-09 16:07:41 +0800

    #1、支持上拉刷新，加载更多

--- lib/ui/rank/project_row.dart
+++ lib/ui/rank/project_row.dart
@@ -1,4 +1,3 @@
-import 'package:code_rank/Theme.dart' as Theme;
 import 'package:code_rank/model/rank_project.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +9,7 @@ class ProjectRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new SizedBox(
-        height: 210.0,
+        height: 120.0,
         child: new Card(
           child: new Container(
               padding: new EdgeInsets.all(10.0),
@@ -18,24 +17,21 @@ class ProjectRow extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   new CircleAvatar(
-                    backgroundImage: new NetworkImage(projectData.owner.avatar_url),
-                    radius: 30.0 ,
-
-
+                    backgroundImage:
+                        new NetworkImage(projectData.owner.avatar_url),
+                    radius: 30.0,
                   ),
                   new Container(
                     padding: new EdgeInsets.all(10.0),
                     child: new Column(
                       children: [
-                      new Text(projectData.name,
-                      style: new TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.black87)),
+                        new Text(projectData.name,
+                            style: new TextStyle(
+                                fontSize: 20.0, color: Colors.black87)),
                         new Expanded(
                           child: new Text(projectData.html_url,
                               style: new TextStyle(
-                                  fontSize: 12.0,
-                                  color: Colors.black87)),
+                                  fontSize: 12.0, color: Colors.black87)),
                         )
                       ],
                     ),
--- lib/ui/rank/rank_list.dart
+++ lib/ui/rank/rank_list.dart
@@ -7,37 +7,120 @@ import 'package:code_rank/ui/rank/project_row.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class RankPage extends StatelessWidget {
+class RankPage extends StatefulWidget {
+  RankPageState createState() => new RankPageState();
+}
+
+class RankPageState extends State<RankPage> {
+  ScrollController _controller;
+  List<RankProject> items;
+  int pageIndex = 1;
+  bool isLoadingMore = false;
+  Future<RankData> _listFuture;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    items = List();
+    _controller = ScrollController();
+    _controller.addListener(_scrollListener);
+    _listFuture = fetchRankProjects();
+  }
+
+  void _scrollListener() {
+    if (_controller.position.pixels == _controller.position.maxScrollExtent) {
+      print("Load More~~~~");
+      pageIndex++;
+      isLoadingMore = true;
+      refreshList();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_scrollListener);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-        body: new FutureBuilder<RankData>(
-            future: fetchRankProjects(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return getListView(snapshot.data.items);
-              } else if (snapshot.hasError) {
-                return new Center(child: new Text("${snapshot.error}"));
-              }
-
-              // By default, show a loading spinner
-              return new Center(
-                child: new CircularProgressIndicator(),
-              );
-            }));
+        body: new RefreshIndicator(
+      onRefresh: () async {
+        pageIndex = 1;
+        isLoadingMore = false;
+        items.clear();
+        refreshList();
+      },
+      child: new FutureBuilder<RankData>(
+          future: _listFuture,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              items.addAll(snapshot.data.items);
+              return createListView();
+            } else if (snapshot.hasError) {
+              return new Center(child: new Text("${snapshot.error}"));
+            } else {
+              return new Center(child: new Text("Loading..."));
+            }
+          }),
+    ));
   }
 
-  ListView getListView(List<RankProject> items) => new ListView.builder(
-      itemCount: items.length,
-      itemBuilder: (BuildContext context, int position) {
-        return new ProjectRow(items[position] as RankProject);
-      });
+  Widget createListView() {
+    return new ListView.builder(
+        controller: _controller,
+        padding: const EdgeInsets.all(16.0),
+        itemCount: items == null ? 0 : items.length,
+        itemBuilder: (context, index) {
+          if (index == items.length) {
+            return _buildProgressIndicator();
+          } else {
+            return ProjectRow(items[index] as RankProject);
+          }
+        });
+  }
+
+  Widget _buildLoadText() {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Center(
+          child: Text("加载中……"),
+        ),
+      ),
+      color: Colors.white70,
+    );
+  }
+
+  Widget _buildProgressIndicator() {
+    return new Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new Center(
+        child: new Opacity(
+          opacity: isLoadingMore ? 1.0 : 0.0,
+          child: new CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
 
   Future<RankData> fetchRankProjects() async {
-    final response = await http.get(
-        'https://api.github.com/search/repositories?q=language:java&sort=stars');
+    String url =
+        'https://api.github.com/search/repositories?q=language:java&sort=stars&page=' +
+            pageIndex.toString();
+    print(url);
+    final response = await http.get(url);
     final responseJson = json.decode(response.body);
     return new RankData.fromJson(responseJson);
+  }
+
+  void refreshList() async {
+    // reload
+    setState(() {
+      _listFuture = fetchRankProjects();
+    });
   }
 }
